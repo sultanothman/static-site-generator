447c7787a688f444feea20e5827cf601aaeb1649
sultanothman <soltanotman@gmail.com>
2024-09-19 10:10:41 +0300

    create main where it will convert markdown to html, and text node that will hold data from markdown files

---
+++ src/textnode.py
@@ -0,0 +1,15 @@
+
+
+class TextNode:
+    def __init__(self, text, text_type, url = None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other) -> bool:
+        if self.text == other.text and  self.text_type == other.text_type and self.url == other.url:
+            return True
+        return False
+    
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
